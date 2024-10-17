bcc482631167dc270d8859a6d316c89ac9391650
Wongani Chulu <wonganichulu@gmail.com>
2024-10-17 20:30:35 +0200

    Added 10-div.sql

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,17 @@
+-- Creates a function SafeDiv
+DELIMITER $$
+CREATE FUNCTION SafeDiv(
+  a INT,
+  b INT
+)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+  IF b = 0 THEN
+    RETURN 0;
+  ELSE
+    RETURN a / b;
+  END IF;
+END;
+$$
+DELIMITER ;
